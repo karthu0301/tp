9f4dc0e31844179a2abe67e11742a5eb516d43f7
Karthika <karthu0301@gmail.com>
2025-02-26 16:54:16 +0800

    Add Remark Command

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -14,8 +14,6 @@ import static seedu.address.testutil.TypicalPersons.BOB;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.testutil.PersonBuilder;
-
 public class PersonTest {
 
     @Test
